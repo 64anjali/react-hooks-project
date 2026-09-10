b60a3bdf4f28c7b9e4a1b4576a93db250b2dfb80
Anjali Garhwal <anjaligarhwal236@gmail.com>
2026-09-10 13:19:49 +0530

    Formatted Counter.jsx with Prettier

--- reacthooksproject/src/components/Counter.jsx
+++ reacthooksproject/src/components/Counter.jsx
@@ -1,27 +1,69 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Counter = () => {
-    const [value, setValue]= useState(1);
-    const handleAdd=()=>{
-        setValue(value+1);
-    }
-    const handleSubtract=()=>{
-        setValue(value-1);
-    }
-    const reset=()=>{
-        setValue(0);
-    }
+  const [value, setValue] = useState(1);
+  const handleAdd = () => {
+    setValue(value + 1);
+  };
+  const handleSubtract = () => {
+    setValue(value - 1);
+  };
+  const reset = () => {
+    setValue(0);
+  };
   return (
-    <div style={{ padding: "50px", backgroundColor:"grey", width:"400px", margin:"auto", textAlign:"center", }}>
-        <h1>Counter Application</h1>
-        <h2 style={{marginTop:"20px", fontSize:"32px", color:"gold"}}>{value}</h2>
-        <div style={{display:"flex", gap:"20px", justifyContent:"center", marginTop:"20px"}}>
-            <button style={{backgroundColor:"green", color:"white", padding:"2px 5px"}} onClick={handleAdd}>Add</button>
-            <button style={{backgroundColor:"red", color:"white", padding:"2px 5px"}} onClick={handleSubtract} disabled={value==0}>Subtract</button>
-             <button style={{backgroundColor:"Blue", color:"white", padding:"2px 5px"}} disabled={value==0} onClick={reset} >Reset</button>
-        </div>
+    <div
+      style={{
+        padding: "50px",
+        backgroundColor: "grey",
+        width: "400px",
+        margin: "auto",
+        textAlign: "center",
+      }}
+    >
+      <h1>Counter Application</h1>
+      <h2 style={{ marginTop: "20px", fontSize: "32px", color: "gold" }}>
+        {value}
+      </h2>
+      <div
+        style={{
+          display: "flex",
+          gap: "20px",
+          justifyContent: "center",
+          marginTop: "20px",
+        }}
+      >
+        <button
+          style={{
+            backgroundColor: "green",
+            color: "white",
+            padding: "2px 5px",
+          }}
+          onClick={handleAdd}
+        >
+          Add
+        </button>
+        <button
+          style={{ backgroundColor: "red", color: "white", padding: "2px 5px" }}
+          onClick={handleSubtract}
+          disabled={value == 0}
+        >
+          Subtract
+        </button>
+        <button
+          style={{
+            backgroundColor: "Blue",
+            color: "white",
+            padding: "2px 5px",
+          }}
+          disabled={value == 0}
+          onClick={reset}
+        >
+          Reset
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Counter
+export default Counter;
